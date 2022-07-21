a64a1c834d3946bee0c13141a6cdfa52921ab367
rcoder3270 <rcoder3270@gmail.com>
2022-07-21 17:54:18 +0800

    command change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express(); 
 
 app.get('/ping', (req, res) => res.send('pong'));
-app.post('/reboot', (req, res) => { exec('reboot'); res.send(); });
-app.post('/shutdown', (req, res) => { exec('shutdown'); res.send(); });
+app.post('/reboot', (req, res) => { exec('sudo reboot'); res.send(); });
+app.post('/shutdown', (req, res) => { exec('sudo poweroff'); res.send(); });
 
 app.listen(6700);
